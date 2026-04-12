07c5fec3482d633a280a03ef2a3d5d5c697c008b
FahadulShadhin <fahadulshadhin17@gmail.com>
2026-04-13 01:50:50 +0600

    pm init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const command = process.argv[2];
+
+switch (command) {
+    case "init":
+        handleInit();
+        break;
+    
+    default:
+        console.log(`Unknown command: ${command}`);
+        console.log("Available commands:");
+        console.log("  pm init");
+        break;
+}
+
+function handleInit() {
+    console.log("🔐 Initializing password manager..."); 
+}
