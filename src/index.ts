2481d15affa7d001fe6d9e706a40c980939bc2be
FahadulShadhin <fahadulshadhin17@gmail.com>
2026-04-13 16:19:17 +0600

    pm unlock

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,22 @@ switch (command) {
     handleInit();
     break;
 
+  case 'unlock':
+    handleUnlock();
+    break;
+
   default:
     console.log(`Unknown command: ${command}`);
     console.log('Available commands:');
     console.log('  pm init');
+    console.log('  pm unlock');
     break;
 }
 
 function handleInit() {
   console.log('🔐 Initializing password manager...');
+}
+
+function handleUnlock() {
+  console.log('🔓 Unlocking password manager...');
 }
